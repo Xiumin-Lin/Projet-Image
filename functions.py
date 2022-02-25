81242fc5da863c0e1a42ef4946602e2c94169227
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2022-02-25 19:05:47 +0100

    Add edge detection process (to be completed)

--- functions.py
+++ functions.py
@@ -1,10 +1,26 @@
 import numpy as np
+import sys
+import os.path
+
+
+# Egaliser
+def egaliser(img):
+    # TODO
+    print("DO Egaliser")
+    return img
+
+
+# Convolution
+def convolution(img, filtre):
+    # TODO
+    print("DO convolution")
+    return img
+
 
 # Otsu (version du prof)
-
-
 def otsu(img):
-    meilleurSeuil = 0
+    print("Use otsu : ")
+    meilleur_seuil = 0
     minimun = 10_000_000_000
 
     histogram = np.zeros(256, dtype=np.uint8)
@@ -43,10 +59,40 @@ def otsu(img):
         for i in range(seuil, 256):
             s2 += ((i - mu2) ** 2) * histogram[i]
 
-        intraClassVar = w1 * s1 + w2 * s2
-        if intraClassVar < minimun:
-            meilleurSeuil = seuil
-            minimun = intraClassVar
+        intra_class_var = w1 * s1 + w2 * s2
+        if intra_class_var < minimun:
+            meilleur_seuil = seuil
+            minimun = intra_class_var
             print(f"Nouveau meilleur seuil : {seuil}")
 
-    print(f"Le meilleur seuil est {meilleurSeuil}")
+    print(f"Le meilleur seuil est {meilleur_seuil}")
+    return meilleur_seuil
+
+
+def seuil(img, seuil):
+    # TODO
+    return img
+
+
+#
+def get_file(prefix):
+    # mul = 1 pour afficher une img .jpeg ou jpg dans sa couleur d'origine
+    # mul = 255 pour afficher une img .png dans sa couleur d'origine
+    filename = ""
+    multiplicateur = 1
+    file_location = "./res/" + str(prefix)
+    if os.path.isfile(file_location + ".jpeg"):
+        filename = str(prefix) + ".jpeg"
+    elif os.path.isfile(file_location + ".jpg"):
+        filename = str(prefix) + ".jpg"
+    elif os.path.isfile(file_location + ".png"):
+        filename = str(prefix) + ".png"
+        multiplicateur = 255
+    elif prefix == "-help":
+        # TODO
+        exit()
+    else:
+        print("il n'y a pas de fichier " + str(prefix) +
+              " .jpg, .jpeg ou .png, utilisez le program avec '-help' pour un detail d'utilisation")
+        exit()
+    return filename, multiplicateur
--- projet_python.py
+++ projet_python.py
@@ -1,44 +1,39 @@
 import numpy as np
 import matplotlib.image as mplimg
 import matplotlib.pyplot as plt
-import sys
-import os.path
+from enum import Enum
 import functions
 
+
+# TODO a complter
+class Filtre(Enum):
+    GAUSSIAN = [0, 0, 0]
+
+
 # Saisie du nom du fichier
-if len(sys.argv) < 2:
-    print("utilisez le program avec l'option '-help' pour un detail d'utilisation")
-    exit()
+# if len(sys.argv) < 2:
+#     print("utilisez le program avec l'option '-help' pour un detail d'utilisation")
+#     exit()
 
-prefix = sys.argv[1]
+# prefix = sys.argv[1]
+for i in range(10):
+    # i represente le num de l'img que vous souhaitez tester
+    filename, multiplicateur = functions.get_file(i)
 
-# mul = 1 pour afficher une img .jpeg ou jpg dans sa couleur d'origine
-# mul = 255 pour afficher une img .png dans sa couleur d'origine
-multiplicateur = 1
+    # recupere l'img
+    print(filename)
+    img = (mplimg.imread("res/" + filename).copy()
+           * multiplicateur).astype(np.uint8)
+    # TODO detecter si trop sombre ou trop
+    img_egalised = functions.egaliser(img)
+    # TODO reduire les bruits (blur)
+    img_blured = functions.convolution(img_egalised, Filtre.GAUSSIAN)
+    # TODO appliquer le seuil
+    # best_seuil = functions.otsu(img_blured)
+    # img_seuil = functions.seuil(img, best_seuil)
 
-if (os.path.isfile("./res/" + prefix + ".jpeg")):
-    filename = prefix + ".jpeg"
-elif(os.path.isfile("./res/" + prefix + ".jpg")):
-    filename = prefix + ".jpg"
-elif(os.path.isfile("./res/" + prefix + ".png")):
-    filename = prefix + ".png"
-    multiplicateur = 255
-elif prefix == "-help":
-    exit()
-else:
-    print("il n'y a pas de fichier " + prefix +
-          " .jpg, .jpeg ou .png, utilisez le program avec '-help' pour un detail d'utilisation")
-    exit()
-
-print(filename)
-
-# recupere l'img
-img = (mplimg.imread("res/" + filename).copy()
-       * multiplicateur).astype(np.uint8)
-
-functions.otsu(img)
-
-plt.figure()
-plt.title(filename)
-plt.imshow(img, cmap=plt.cm.gray)
-plt.show()
+    plt.figure()
+    plt.title(filename)
+    # plt.imshow(img_seuil, cmap=plt.cm.gray)
+    plt.imshow(img_blured, cmap=plt.cm.gray)
+    plt.show()
